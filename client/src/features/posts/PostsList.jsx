f65379e09cc1c1b559b5bd25a73e763b1b28b98f
Jetulio <joo_ramos2006@hotmail.com>
2024-06-19 17:01:52 -0300

    Little fix

--- client/src/features/posts/PostsList.jsx
+++ client/src/features/posts/PostsList.jsx
@@ -34,7 +34,7 @@ function PostsList() {
         <div>
             {posts.map((post) => (
                 <div key={post.id} className="post-container">
-                    <h2>{post.title}<spam><Link to="posts/:id"> Edit</Link></spam></h2>
+                    <h2>{post.title}</h2>
                     <p>{post.body}</p>                    
                 </div>
             ))}
