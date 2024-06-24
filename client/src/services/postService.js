be51eb2dfaf4bef99285fc6b4b62e0b93bfebf85
Jetulio <joo_ramos2006@hotmail.com>
2024-06-24 12:34:53 -0300

    Finished the refactor.

--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -16,6 +16,38 @@ async function fetchPost(id) {
   }
   return response.json();
 }
+// Create a post
+async function createPost(postData) {
+  const response = await fetch(`${API_URL}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(postData),
+  });
+
+  if (!response) {
+    throw new Error(response.statusText);
+  }
+
+  return response.json();
+}
+// Update a post
+async function updatePost(id, postData) {
+  const response = await fetch(`${API_URL}/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(postData),
+  });
+
+  if (!response) {
+    throw new Error(response.statusText);
+  }
+
+  return response.json();
+}
 // Delete a post
 async function deletePost(id) {
   const response = await fetch(`${API_URL}/${id}`, {
@@ -29,4 +61,4 @@ async function deletePost(id) {
   return response.json();
 }
 
-export { deletePost, fetchAllPosts, fetchPost };
+export { createPost, deletePost, fetchAllPosts, fetchPost, updatePost };
